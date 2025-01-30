a50c25af5a0c2b152cd6ff5a5ed0350b5726d86e
Izabella Hermano <izabellahermano1@gmail.com>
2025-01-30 11:14:43 -0300

    Introdução POO

---
+++ src/main/java/com/senai/aula01_introducao_poo/exemplos/pessoa/Main.java
@@ -0,0 +1,10 @@
+package com.senai.aula01_introducao_poo.exemplos.pessoa;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pessoa pessoa01 = new Pessoa();
+        pessoa01.nome = "Hermano";
+        System.out.println(pessoa01.nome);
+    }
+}
