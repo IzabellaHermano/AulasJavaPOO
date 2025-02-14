d53b0786516f93af1579e266c0c7536927009031
Izabella Hermano <izabellahermano1@gmail.com>
2025-02-14 10:59:55 -0300

    Aula 02 - Coleções

--- src/main/java/com/senai/aula02_colecoes/exemplos/pessoa/Main.java
+++ src/main/java/com/senai/aula02_colecoes/exemplos/pessoa/Main.java
@@ -1,12 +1,14 @@
 package com.senai.aula02_colecoes.exemplos.pessoa;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Pessoa pessoa1 = new Pessoa("Izabella", 18);
-        Pessoa pessoa2 = new Pessoa("Pietra", 17);
-        Pessoa pessoa3 = new Pessoa("Ana", 15);
+        Pessoa pessoa2 = new Pessoa("Pietra", 20);
+        Pessoa pessoa3 = new Pessoa("Ana", 10);
 
         Pessoa[] matrizPessoas = new Pessoa[4];
 
@@ -43,6 +45,33 @@ public class Main {
         listar(listaPessoas);
         listaPessoas.remove(pessoa1);
         listar(listaPessoas);
+
+        listaPessoas.set(0,pessoa2);
+        listar(listaPessoas);
+
+        listaPessoas.add(0,pessoa3);
+        listar(listaPessoas);
+
+        System.out.println("-----------teste------------");
+        listaPessoas.forEach(System.out::println);
+        List<Pessoa> listaFiltrada = listaPessoas.stream().filter(p -> p.idade >=10).toList();
+        System.out.println("--------------lista filtrada----------------------");
+        listaFiltrada.forEach(System.out::println);
+
+        listaPessoas.add(new Pessoa("Carolina", 24));
+
+        listaFiltrada = listaPessoas.stream().filter(p ->p.nome.toLowerCase().contains("na")).toList();
+        System.out.println("--------------teste-----------------");
+        listaFiltrada.forEach(System.out::println);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Preencha os dados a seguir: ");
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine();
+        System.out.print("Idade: ");
+        int idade = scanner.nextInt();
+        listaPessoas.add(new Pessoa(nome,idade));
+        listaPessoas.forEach(System.out::println);
 
     }
 
