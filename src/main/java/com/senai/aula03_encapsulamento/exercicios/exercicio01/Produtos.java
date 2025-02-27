7f58c57511225be035c2248d5f1f043f95bbc523
Izabella Carolina <izabellahermano1@gmail.com>
2025-02-27 10:55:16 -0300

    Exercicio01  - Aula03

---
+++ src/main/java/com/senai/aula03_encapsulamento/exercicios/exercicio01/Produtos.java
@@ -0,0 +1,27 @@
+package com.senai.aula03_encapsulamento.exercicios.exercicio01;
+
+public class Produtos {
+    private String nome;
+    private double preco;
+
+    public Produtos(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        if (preco >= 0) {
+            this.preco = preco;
+        } else {
+            System.out.println("O preço não pode ser negativo.");
+        }
+    }
+}
