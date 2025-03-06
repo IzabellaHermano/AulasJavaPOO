3e983430bc725627d3c5378c609f4a193801bd37
Izabella Hermano <izabellahermano1@gmail.com>
2025-03-06 11:03:33 -0300

    Aulas Java POO

--- src/main/java/com/senai/aula03_encapsulamento/exemplos/conta_bancária_com_operações_simples/ContaBancaria.java
+++ src/main/java/com/senai/aula03_encapsulamento/exemplos/conta_bancária_com_operações_simples/ContaBancaria.java
@@ -1,4 +1,56 @@
 package com.senai.aula03_encapsulamento.exemplos.conta_bancária_com_operações_simples;
 
 public class ContaBancaria {
+
+    private String titular;
+    private double saldo;
+
+    public ContaBancaria(String titular, double saldo) {
+        this.titular = titular;
+        if ( saldo>= 0) {
+            this.saldo = saldo;
+        }else {
+            System.out.println("O saldo inicial não pode ser negativo!");
+            System.out.println("O saldo da conta iniciará com R$0,00.");
+        }
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor){
+        if (valor>0){
+            saldo += valor;
+            System.out.println("Deposito realizado com sucesso do titular "+titular+"! \nSaldo Atual R$"+saldo);
+
+        }else {
+            System.out.println("Valor inválido! O valor precisa ser maior que zero");
+        }
+
+    }
+    public void sacar(double valor){
+        if (valor<= saldo){
+            saldo -= valor;
+            System.out.println("Saque realizado com sucesso do titular "+titular+"! \nSaldo Atual R$"+saldo);
+        }else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ContaBancaria{" +
+                "titular='" + titular + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
 }
--- src/main/java/com/senai/aula03_encapsulamento/exemplos/conta_bancária_com_operações_simples/Main.java
+++ src/main/java/com/senai/aula03_encapsulamento/exemplos/conta_bancária_com_operações_simples/Main.java
@@ -1,4 +1,20 @@
 package com.senai.aula03_encapsulamento.exemplos.conta_bancária_com_operações_simples;
 
 public class Main {
+    public static void main(String[] args) {
+        ContaBancaria contaBancaria01 = new ContaBancaria("Izabella", -100);
+        ContaBancaria contaBancaria02 = new ContaBancaria("Ana", 1000000);
+
+        System.out.println(contaBancaria01);
+        System.out.println(contaBancaria02);
+
+        System.out.println("______Teste Depositar______");
+
+        contaBancaria01.depositar(100);
+        contaBancaria02.depositar(-1000000);
+
+        System.out.println("______Teste Sacar______");
+        contaBancaria01.sacar(110);
+        contaBancaria02.sacar(50);
+    }
 }
