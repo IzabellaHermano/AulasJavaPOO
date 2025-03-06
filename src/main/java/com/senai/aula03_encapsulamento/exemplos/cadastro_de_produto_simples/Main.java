a100994b388e2307cf12b9dce49b5fac5588293e
Izabella Hermano <izabellahermano1@gmail.com>
2025-03-06 10:28:33 -0300

    Aulas Java POO

--- src/main/java/com/senai/aula03_encapsulamento/exemplos/cadastro_de_produto_simples/Main.java
+++ src/main/java/com/senai/aula03_encapsulamento/exemplos/cadastro_de_produto_simples/Main.java
@@ -2,16 +2,24 @@ package com.senai.aula03_encapsulamento.exemplos.cadastro_de_produto_simples;
 
 public class Main {
     public static void main(String[] args) {
-        Produto produto01 = new Produto("Queijo", 20.9);
+        Produto produto01 = new Produto("Queijo", -20.9);
         Produto produto02 = new Produto("Iogurte", 5.9);
 
         System.out.println(produto01);
         System.out.println(produto02);
 
-        int a = 2;
-        int b = 1;
+        System.out.println("______________________________");
+        produto01.setPreco(21.9);
+        produto02.setPreco(-4.9);
 
-        System.out.println(produto01 == produto02);
+        System.out.println(produto01);
+        System.out.println(produto02);
+
+        //System.out.println(produto01.equals(produto02));
+        //equals é o == (comparação) da orientação a objeto
+        //equals: compara referencias
+        //implementar: fazer o codigo
+
 
     }
 }
--- src/main/java/com/senai/aula03_encapsulamento/exemplos/cadastro_de_produto_simples/Produto.java
+++ src/main/java/com/senai/aula03_encapsulamento/exemplos/cadastro_de_produto_simples/Produto.java
@@ -1,12 +1,19 @@
 package com.senai.aula03_encapsulamento.exemplos.cadastro_de_produto_simples;
 
+import java.util.Objects;
+
 public class Produto {
     private String nome;
     private double preco;
 
     public Produto(String nome, double preco) {
         this.nome = nome;
-        this.preco = preco;
+        if ( preco>=0) {
+            this.preco = preco;
+        }else {
+            System.out.println("VALOR INVÁLIDO!\nO preço do produto não pode ser negativo.");
+            System.out.println("O preço do produto será atribuido como R$0,00");
+        }
     }
 
     public String getNome() {
@@ -22,7 +29,27 @@ public class Produto {
     }
 
     public void setPreco(double preco) {
-        this.preco = preco;
+        this.nome = nome;
+        if ( preco>=0) {
+            this.preco = preco;
+        }else {
+            System.out.println("VALOR INVÁLIDO!\nO preço do produto não pode ser negativo.");
+        }
+    }
+    //construção do equals e hashCode possibilitando comparação
+    @Override //Override: substitui metodo da classe "pai", sobrescreve
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false; //testa se é nulo ou se esta na mesma classe
+        Produto produto = (Produto) o;//realizando cashting
+        return Double.compare(preco, produto.preco) == 0 && Objects.equals(nome, produto.nome);//comparando atributos da classe criada(um por um)
+        //se não gerar equals, a comparação sera entre os "caminhos" e não dos conteudos dos atributos
+    }
+
+    @Override
+    //hashCode: Agrupa codigos
+    //Determina qual grupo(bucket) cada objeto ira pertencer
+    public int hashCode() {
+        return Objects.hash(nome, preco);
     }
 
     @Override
