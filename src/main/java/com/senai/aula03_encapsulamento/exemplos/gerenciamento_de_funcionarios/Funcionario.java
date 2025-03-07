3dd6d1cd2c06d3f313ddc6dc054a0110220c19c8
Izabella Hermano <izabellahermano1@gmail.com>
2025-03-07 11:10:04 -0300

    Aulas Java POO

--- src/main/java/com/senai/aula03_encapsulamento/exemplos/gerenciamento_de_funcionarios/Funcionario.java
+++ src/main/java/com/senai/aula03_encapsulamento/exemplos/gerenciamento_de_funcionarios/Funcionario.java
@@ -1,13 +1,14 @@
 package com.senai.aula03_encapsulamento.exemplos.gerenciamento_de_funcionarios;
 
+import java.util.Arrays;
+
 public class Funcionario {
     private String nome;
     private double salario;
     private String cargo;
-    private String[] listaCargos ={ "Gerente",
-            "Analista", "Programador"};
+    private String[] listaCargos ={ "Gerente","Analista", "Programador"};
 
-    public Funcionario(String nome, double salario, String cargo) {
+    public Funcionario(String nome, double salario, int idCargo) {
 
         if(!nome.isBlank()){
         this.nome = nome;
@@ -22,8 +23,14 @@ public class Funcionario {
             System.out.println("O campo salário será atribuido como R$1320,00");
             this.salario = 1320;
         }
+        if(idCargo >= 1 && idCargo <= listaCargos.length){
+            this.cargo = listaCargos[idCargo - 1];
+        }else {
+            System.out.println("Cargo Inválido!");
+            System.out.println("O campo cargo será atribuido como 'Programador'");
+            this.cargo = listaCargos[2];
+        }
 
-        this.cargo = cargo;
     }
 
     public String getNome() {
@@ -31,7 +38,11 @@ public class Funcionario {
     }
 
     public void setNome(String nome) {
-        this.nome = nome;
+        if(!nome.isBlank()){
+            this.nome = nome;
+        }else {
+            System.out.println("O campo nome não pode ser nulo ou vazio.");
+        }
     }
 
     public double getSalario() {
@@ -39,14 +50,38 @@ public class Funcionario {
     }
 
     public void setSalario(double salario) {
-        this.salario = salario;
+        if (salario>=1320){
+            this.salario = salario;
+        }else {
+            System.out.println("O salário deve ser igual ou superior a R$1320,00");
+        }
+    }
+    public void aumentarSalario(int porcentagemAumento){
+        if (porcentagemAumento>0) {
+            this.salario += (salario * porcentagemAumento) / 100;
+            System.out.println("Aumento do salário do funcionario "+nome+" realizado com sucesso | Valor atualizado: R$"+salario);
+        }else {
+            System.out.println("Valor Inválido!");
+        }
+
     }
 
     public String getCargo() {
         return cargo;
     }
 
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
+    public void setCargo(int idCargo) {
+        if(idCargo >= 1 && idCargo <= listaCargos.length){
+            this.cargo = listaCargos[idCargo - 1];
+        }else {
+            System.out.println("Cargo Inválido!");
+            System.out.println("O campo cargo será atribuido como 'Programador'");
+            this.cargo = listaCargos[2];
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionário: \n"+ "NOME: "+ nome + "\n"+ "CARGO: "+cargo+ "\n"+ "SALÁRIO: "+ salario;
     }
 }
--- src/main/java/com/senai/aula03_encapsulamento/exemplos/gerenciamento_de_funcionarios/Main.java
+++ src/main/java/com/senai/aula03_encapsulamento/exemplos/gerenciamento_de_funcionarios/Main.java
@@ -1,4 +1,21 @@
 package com.senai.aula03_encapsulamento.exemplos.gerenciamento_de_funcionarios;
 
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("____________TESTE 01____________");
+        Funcionario funcionario01 = new Funcionario("Izabella",2000,1);
+        System.out.println(funcionario01);
+
+        System.out.println("____________TESTE 02____________");
+        Funcionario funcionario02 = new Funcionario("",1200,4);
+        System.out.println(funcionario02);
+
+        System.out.println("____________TESTE 03____________");
+        funcionario01.aumentarSalario(60);
+
+
+    }
+
+
 }
