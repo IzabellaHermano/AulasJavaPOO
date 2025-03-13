9490106383f004e91fd6cdd337bdaf2342517d7b
Izabella Hermano <izabellahermano1@gmail.com>
2025-03-13 11:09:33 -0300

    Aula 04 - Herança

--- src/main/java/com/senai/aula04_heranca/exemplos/gerenciamento_conta_bancaria/ContaPoupanca.java
+++ src/main/java/com/senai/aula04_heranca/exemplos/gerenciamento_conta_bancaria/ContaPoupanca.java
@@ -1,4 +1,13 @@
 package com.senai.aula04_heranca.exemplos.gerenciamento_conta_bancaria;
 
-public class ContaPoupanca {
+public class ContaPoupanca extends ContaBancaria{
+    private double taxaRendimento;
+
+    public ContaPoupanca(String titular, double saldo, double taxaRendimento) {
+        super(titular, saldo);
+        this.taxaRendimento = taxaRendimento;
+    }
+    public void  aplicarRendimento(){
+        saldo+=(saldo*taxaRendimento)/100;
+    }
 }
--- src/main/java/com/senai/aula04_heranca/exemplos/gerenciamento_conta_bancaria/Main.java
+++ src/main/java/com/senai/aula04_heranca/exemplos/gerenciamento_conta_bancaria/Main.java
@@ -1,5 +1,8 @@
 package com.senai.aula04_heranca.exemplos.gerenciamento_conta_bancaria;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 
 }
