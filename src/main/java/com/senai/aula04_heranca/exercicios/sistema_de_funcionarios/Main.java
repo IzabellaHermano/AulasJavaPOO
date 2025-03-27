45535cf4b715116cfbcf3e6daa703adf23a6ecfc
Izabella Hermano <izabellahermano1@gmail.com>
2025-03-27 11:12:52 -0300

    Aula 04 - Herança

--- src/main/java/com/senai/aula04_heranca/exercicios/sistema_de_funcionarios/Main.java
+++ src/main/java/com/senai/aula04_heranca/exercicios/sistema_de_funcionarios/Main.java
@@ -21,8 +21,29 @@ public class Main {
         Professor [] professoresLinguagens = {
                 new Professor("Joana",4500.00,"Português"),
                 new Professor("Ricardo", 4500.00,"Inglês")
-
         };
+        ArrayList<String> equipe1 = new ArrayList<>();
+        for (Professor professor : professoresExatas) {
+            professor.exibirDadosFuncionario();
+            equipe1.add(professor.getNome());
+        }
+        ArrayList<String> equipe2 = new ArrayList<>();
+        for (Professor professor : professoresHumanas) {
+            professor.exibirDadosFuncionario();
+            equipe2.add(professor.getNome());
+        }
+        ArrayList<String> equipe3 = new ArrayList<>();
+        for (Professor professor : professoresLinguagens) {
+            professor.exibirDadosFuncionario();
+            equipe3.add(professor.getNome());
+        }
+        Coordenador coordenador1 = new Coordenador("Carlos", 6000.00, equipe1);
+        Coordenador coordenador2 = new Coordenador("Fernanda", 6000.00, equipe2);
+        Coordenador coordenador3 = new Coordenador("Patrícia", 6000.00, equipe3);
+        System.out.println("\nDados dos Coordenadores:");
+        coordenador1.exibirDadosFuncionario();
+        coordenador2.exibirDadosFuncionario();
+        coordenador3.exibirDadosFuncionario();
 
 
     }
